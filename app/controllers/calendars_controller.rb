869a13bbcf8d6d7c1c25990ec068e3dda668d4e3
Nicolas Delrez <nicolas.delrez@gmail.com>
2012-12-12 12:00:06 +0100

    go

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,14 +15,10 @@ class CalendarsController < ApplicationController
           render :pdf => "file_name",
                  :template => 'calendars/show.pdf.erb',
                  :layout => 'pdf',
-                 :page_size  => 'A3', 
-                 #:show_as_html => true,
+                 :page_size  => 'A3',
+                 :show_as_html => true
                  #:save_to_file  => Rails.root.join('pdfs', "#{@calendar.name}.pdf"),
-                 :footer => {
-                    :center => "Center",
-                    :left => "Left",
-                    :right => "Right"
-                 }
+                 
       end
     end    
   end
