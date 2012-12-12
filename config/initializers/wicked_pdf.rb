04aec19f71b7c86fa2023b02bef93de0d5b6a471
Nicolas Delrez <nicolas.delrez@gmail.com>
2012-12-12 09:55:55 +0100

    go

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,11 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => 'C:\htmltopdf\wkhtmltopdf\wkhtmltopdf.exe'
+  
+  if Rails.env.production?
+    :exe_path => Rails.root.join('lib', 'wkhtmltopdf-amd64').to_s
+  else
+    :exe_path => 'C:\htmltopdf\wkhtmltopdf\wkhtmltopdf.exe'
+  end
+  
 }
