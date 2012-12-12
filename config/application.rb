a60cf2640304951ad1b2c0851fab98e06e02a5c9
Nicolas Delrez <nicolas.delrez@gmail.com>
2012-12-12 15:16:59 +0100

    go

--- config/application.rb
+++ config/application.rb
@@ -31,9 +31,7 @@ module Agoria
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = "fr"  
-    
-    
+    config.i18n.default_locale = :fr    
  
     config.autoload_paths += %W(#{config.root}/lib)
     
