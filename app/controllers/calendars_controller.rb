c3e9981217531ced2e66b2dc2ffc2d51123d7e59
Nicolas Delrez <nicolas.delrez@gmail.com>
2012-12-13 09:58:13 +0100

    test

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -12,7 +12,7 @@ class CalendarsController < ApplicationController
     @calname = @calendar.name+'_'+@calendar.id.to_s
     respond_to do |format|
       format.pdf do
-          render :page_size => "A3",
+          render :page_size => "A4",
                   :pdf => @calname,
                   #:disposition => 'attachment',
                   :template => 'calendars/show.pdf.erb',
