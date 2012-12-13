84ab7e43849f791f8e72b1a69352236bcc0bf080
Nicolas Delrez <nicolas.delrez@gmail.com>
2012-12-13 08:37:46 +0100

    go

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -12,21 +12,18 @@ class CalendarsController < ApplicationController
     @calname = @calendar.name+'_'+@calendar.id.to_s
     respond_to do |format|
       format.pdf do
-          render 
-            :page_size => 'Letter',
-            :pdf => @calname,
-             #:disposition => 'attachment',
-             :page_height => '16.5in', 
-             :page_width => '11.7in',
-             :dpi => 300,
-             :template => 'calendars/show.pdf.erb',
-             :save_to_file  => Rails.root.join('public/pdfs', "#{@calname}.pdf"),
-             :margin => {:top   => 5,
-                       :bottom  => 5,
-                       :left    => 5,
-                       :right   => 5},
-             :no_background => false,
-             :lowquality  => true         
+          render :page_size => 'A1',
+                  :pdf => @calname,
+                  #:disposition => 'attachment',
+                  :dpi => 300,
+                  :template => 'calendars/show.pdf.erb',
+                  :save_to_file  => Rails.root.join('public/pdfs', "#{@calname}.pdf"),
+                  :margin => {:top   => 5,
+                             :bottom  => 5,
+                             :left    => 5,
+                             :right   => 5},
+                  :no_background => false,
+                  :lowquality  => true         
       end
     end    
   end
