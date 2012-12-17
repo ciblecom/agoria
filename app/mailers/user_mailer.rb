798c5ed8e0699e9fd7660d6144cb44c41abf6b54
Nicolas Delrez <nicolas.delrez@gmail.com>
2012-12-17 17:04:29 +0100

    go

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "no-reply@ensemblepourmonemploi.be"
   
   def welcome_email(user)
-    mail(:to => "nicolas.delrez@cible.be", :subject => "Nouvelle inscription sur ensemblepourmonemploi.be")
+    mail(:to => "thierry.castagne@agoria.be,nicolas.delrez@cible.be", :subject => "Nouvelle inscription sur ensemblepourmonemploi.be")
   end
   
   def active_email(user)
